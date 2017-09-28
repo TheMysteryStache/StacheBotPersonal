e3a930350108db0ce84bb82bf2c30b4acecca48c
TheMysteryStache <danielabdi32@gmail.com>
2017-09-28 12:54:16 -0400

    Add files via upload

--- Stachebotpersonal.py
+++ Stachebotpersonal.py
@@ -3,8 +3,11 @@ import discord
 from discord.ext import commands
 import random
 import re
+import os
+
 bot = commands.Bot(command_prefix='.', description='JeffBot')
 client = discord.Client
+
 
 @bot.event
 async def on_ready():
@@ -19,27 +22,33 @@ async def on_message(message):
 
     if re.search("jeff", message.content, flags=0):
         tmp = await bot.send_message(message.channel, 'HAHAHAHAHA FUNNY MEME')
-    if re.search("<@!244933944175362048>", message.content, flags=0):
+    if re.search("<@244933944175362048>", message.content, flags=0):
         tmp = await bot.send_message(message.channel, 'AY DONT PING MY CREATOR')
     if re.search("retard|gay|fag|faggot|ass|thot|fuck|FUCK|fucc|succ|dinlo|homo|fuk|nigger" , message.content, flags = 0):
         tmp = await bot.send_message(message.channel, 'NO BAD WORDS IN MY CHRISTIAN DISCORD')
     if re.search("dog", message.content, flags=0):
         tmp = await bot.send_message(message.channel, "Dogs aren't real duh")
     if re.search(".commie", message.content, flags = 0):
-        with open('Desktop/spaghettibot/commie.png', 'rb') as f:
+        with open('C:\\Users\\Computer Science\\Desktop\\StacheBotPersonal-master\\StacheBotPersonal-master\\commie.png', 'rb') as f:
             tmp = await bot.send_file(message.channel, f)
+
 
     await bot.process_commands(message)
 
 
 @bot.command()
-async def roll(self):
+async def roll():
     a = random.randint(1, 20)
     await bot.say(a)
 
 @bot.command()
-async def ping(self):
+async def ping():
     await bot.say("Pong!")
+
+@bot.group()
+async def play():
+    print (jeff)
+
 
 
 
